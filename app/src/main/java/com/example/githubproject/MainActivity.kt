75a63ddda4ec8f114514bbe1df4eb03f17d6c601
Kevin <kevin.lam09@gmail.com>
2020-07-23 08:17:30 -0400

    Added interface for callback

--- app/src/main/java/com/example/githubproject/MainActivity.kt
+++ app/src/main/java/com/example/githubproject/MainActivity.kt
@@ -2,11 +2,14 @@ package com.example.githubproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.fragment.app.Fragment
 import com.example.githubproject.search.SearchContract
+import com.example.githubproject.search.SearchInputFragment
 import com.example.githubproject.search.SearchPresenter
 import javax.inject.Inject
 
-class MainActivity : AppCompatActivity(), SearchContract.View {
+class MainActivity : AppCompatActivity(), SearchInputFragment.OnSearchSubmittedListener{
 //    @Inject
 //    protected lateinit var presenter: SearchPresenter
 
@@ -16,6 +19,17 @@ class MainActivity : AppCompatActivity(), SearchContract.View {
 
         val applicationComponent = GitHubProjectApplication.get(this).getAppComponent()
 
+    }
+
+    override fun onAttachFragment(fragment: Fragment) {
+        super.onAttachFragment(fragment)
+        if (fragment is SearchInputFragment) {
+            fragment.setOnHeadlineSelectedListener(this)
+        }
+    }
+
+    override fun onSearchSubmitted(searchValue: String) {
+        Toast.makeText(this, searchValue, Toast.LENGTH_SHORT).show();
     }
 
 }
--- app/src/main/java/com/example/githubproject/search/SearchInputFragment.kt
+++ app/src/main/java/com/example/githubproject/search/SearchInputFragment.kt
@@ -6,8 +6,11 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.githubproject.R
+import kotlinx.android.synthetic.main.layout_search_input.*
 
 class SearchInputFragment : Fragment() {
+    internal var callback: OnSearchSubmittedListener? = null
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -15,5 +18,30 @@ class SearchInputFragment : Fragment() {
     ): View {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.layout_search_input, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initButton()
+    }
+
+    private fun initButton() {
+        searchButton.setOnClickListener {
+            val searchInput = search_input_edit_text.text
+            callback?.onSearchSubmitted(searchInput.toString())
+        }
+    }
+
+    fun setOnHeadlineSelectedListener(callback: OnSearchSubmittedListener) {
+        this.callback = callback
+    }
+
+    fun onSearchClick(view:View) {
+
+    }
+
+    interface OnSearchSubmittedListener {
+        fun onSearchSubmitted(searchValue: String)
     }
 }
