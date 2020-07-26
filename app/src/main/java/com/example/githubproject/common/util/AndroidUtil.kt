d8c75e4593f589e87cc23b3dec99e83adb8fc195
Kevin <kevin.lam09@gmail.com>
2020-07-25 23:50:40 -0400

    Minor formatting

--- app/src/main/java/com/example/githubproject/common/util/AndroidUtil.kt
+++ app/src/main/java/com/example/githubproject/common/util/AndroidUtil.kt
@@ -4,12 +4,10 @@ import android.content.Context
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 
-
 class AndroidUtil {
     companion object {
         fun hideSoftKeyboard(context: Context, view: View) {
-            val imm =
-                context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
             imm.hideSoftInputFromWindow(view.windowToken, 0)
         }
     }
